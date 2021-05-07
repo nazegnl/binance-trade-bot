744a165db56428b84aa256a66d717f768420cdc3
nazegnl <nazegnl@protonmail.com>
2021-05-07 10:41:52 +0200

    Database return to normal sessions

--- binance_trade_bot/database.py
+++ binance_trade_bot/database.py
@@ -8,7 +8,7 @@ from typing import List, Optional, Union
 from socketio import Client
 from socketio.exceptions import ConnectionError as SocketIOConnectionError
 from sqlalchemy import create_engine, func
-from sqlalchemy.orm import Session, scoped_session, sessionmaker
+from sqlalchemy.orm import Session
 
 from .config import Config
 from .logger import Logger
